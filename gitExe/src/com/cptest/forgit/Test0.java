e0e002ea02910d9a62cf9b31a01f1f831d4c597e
noobchenp <1349647201@qq.com>
2022-05-30 14:15:44 +0800

    尝试推送修改

--- gitExe/src/com/cptest/forgit/Test0.java
+++ gitExe/src/com/cptest/forgit/Test0.java
@@ -3,5 +3,7 @@ package com.cptest.forgit;
 public class Test0 {
     public static void main(String[] args) {
         System.out.println("my test");
+
+        System.out.println("第一次成功");
     }
 }
